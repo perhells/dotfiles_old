50de08f07c906b893c685eb8dc68a825fd72944c
Per Hellström <per.hellstrom@dewire.com>
2017-06-08 09:19:48 +0200

    Add docker clean alias

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -116,3 +116,5 @@ else
     echo "No difference between new and old mirrorlist"
 end;
 end
+
+function docker-clean; docker stop (docker ps -aq); docker rm (docker ps -aq); docker rmi (docker images -aq) --force; docker volume rm (docker volume ls -q); docker ps -a; docker images -a; docker volume ls; end
